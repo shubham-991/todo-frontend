52a067a6ec47c8180888ce104126b06968e0d6db
shubham-991 <dohaiyashubham991@gmail.com>
2023-04-15 06:11:31 +0530

    m

--- src/components/Authentication/Signup/Signup.tsx
+++ src/components/Authentication/Signup/Signup.tsx
@@ -1,80 +1,20 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import './Signup.css';
-// import { useNavigate } from 'react-router-dom';
-
-// const Signup: React.FC = () => {
-//   const [name, setName] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [error, setError] = useState('');
-//   const history = useNavigate();
-
-//   const handleSubmit = async (event: React.FormEvent) => {
-//     event.preventDefault();
-//     try {
-//       const {data} = await axios.post<{ token: string }>('/api/auth/signup', {
-//         name,
-//         email,
-//         password,
-//       });
-//       if (data) {
-//         localStorage.setItem("userInfo", JSON.stringify(data));
-//         history('/todos');
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       setError('Registration failed. Please try again.');
-//     }
-//   };
-
-//   return (
-//     <div className="signup">
-//       <h2>Sign Up</h2>
-//       <form onSubmit={handleSubmit}>
-//         {error && <div className="error">{error}</div>}
-//         <input
-//           type="text"
-//           placeholder="Name"
-//           value={name}
-//           onChange={(e) => setName(e.target.value)}
-//         />
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//         <button type="submit"><h3>SignUp</h3></button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Signup;
 import React, { useState } from 'react';
 import axios from 'axios';
 import './Signup.css';
 import { useNavigate } from 'react-router-dom';
-import Spinner from '../../Spinner/Spinner'; // import a loading spinner component
+import Spinner from '../../Spinner/Spinner';
 
 const Signup: React.FC = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [showSpinner, setShowSpinner] = useState(false); // add new state variable
+  const [showSpinner, setShowSpinner] = useState(false); 
   const history = useNavigate();
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    setShowSpinner(true); // show the spinner when the signup button is clicked
+    setShowSpinner(true); 
     try {
       const {data} = await axios.post<{ token: string }>('/api/auth/signup', {
         name,
@@ -89,7 +29,7 @@ const Signup: React.FC = () => {
       console.error(error);
       setError('Registration failed. Please try again.');
     }
-    setShowSpinner(false); // hide the spinner after the signup process is completed
+    setShowSpinner(false);
   };
 
   return (
